50f009f7a2b7cb3c269bccc06d39ff822527f2e6
IsabelNzioka <isabelnzioka@gmail.com>
2024-03-12 05:12:01 +0300

    UI revamp
    - border layout
    - created new main page application

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -6,44 +6,52 @@
  * TODO - Replace this content of this view to suite the needs of your application.
  */
 Ext.define('MsTraining.view.main.Main', {
-    extend: 'Ext.tab.Panel',
+    extend: 'Ext.container.Container',
     xtype: 'app-main',
-
     requires: [
         'Ext.window.MessageBox',
         'MsTraining.view.main.MainController',
-        'MsTraining.view.main.MainModel'
+        'MsTraining.view.main.MainModel',
     ],
-
-    controller: 'main',
     plugins: 'viewport',
+    controller: 'main',
     viewModel: 'main',
 
     layout: {
         type: 'border'
     },
-
     items: [{
-        xtype: 'panel',
+        xtype: 'mainmenu',
         bind: {
             title: '{name}'
         },
         region: 'west',
-        html: '<ul><li>This area is commonly used for navigation, for example, using a "tree" component.</li></ul>',
         width: 250,
         split: true,
-        tbar: [{
-            text: 'Logout',
-            handler: 'onLogout'
-        }]
-    },{
+    },
+    {
         region: 'center',
         xtype: 'tabpanel',
-        items:[{
-            title: 'Tab 1',
-            html: '<h2>Content appropriate for the current navigation.</h2>'
+        items: [{
+            title: 'Foo'
+        }, 
+        {
+            title: 'Bar',
+            closable:true,
+            tabConfig: {
+                title: 'Custom Title',
+                tooltip: 'A button tooltip'
+            }
         }]
-    }]
-
-    
+       
+    },
+    {
+        region: 'south',
+        xtype: 'appfooter',
+    } ,
+     {
+        region: 'north',
+         xtype: 'appheader',
+    }
+]
 });
